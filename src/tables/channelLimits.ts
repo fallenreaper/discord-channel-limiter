fadab20b9cca109e443c27f71c9982ce88ce4f21
fallenreaper <wllm.francis@gmail.com>
2025-08-17 17:33:02 -0400

    added 2 tables for storing data

---
+++ src/tables/channelLimits.ts
@@ -0,0 +1,21 @@
+import { Table } from "@ghom/orm";
+
+export interface ChannelLimits {
+  channel_id: string;
+  guild_id: string;
+  cost: number;
+}
+
+/**
+ * See the {@link https://ghom.gitbook.io/bot.ts/usage/use-database database guide} for more information.
+ */
+export default new Table<ChannelLimits>({
+  name: "channelLimits",
+  description: "costs incurred for specific channels",
+  setup: (table) => {
+    // setup table columns => https://knexjs.org/guide/schema-builder.html
+    table.string("channel_id").notNullable();
+    table.string("guild_id").notNullable();
+    table.float("cost").notNullable();
+  },
+});
---
+++ src/tables/userInformation.ts
@@ -0,0 +1,19 @@
+import { Table } from "@ghom/orm"
+
+export interface UserInformation {
+  user_id: string
+  allowance: number
+}
+
+/**
+ * See the {@link https://ghom.gitbook.io/bot.ts/usage/use-database database guide} for more information.
+ */
+export default new Table<UserInformation>({
+  name: "userInformation",
+  description: "user metadata for character allowance",
+  setup: (table) => {
+    // setup table columns => https://knexjs.org/guide/schema-builder.html
+    table.string("user_id").notNullable().unique()
+    table.integer("allowance").notNullable()
+  },
+})
