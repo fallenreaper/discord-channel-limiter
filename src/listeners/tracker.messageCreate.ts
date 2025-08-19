75f8abc753ca81e52f72117e666b8eef5a31b2d7
fallenreaper <wllm.francis@gmail.com>
2025-08-18 22:03:12 -0400

    linter

--- src/listeners/tracker.messageCreate.ts
+++ src/listeners/tracker.messageCreate.ts
@@ -1,65 +1,65 @@
-import { INITIAL_POINTS } from "#config";
-import { Listener } from "#core/listener";
-import channelLimits from "#tables/channelLimits";
-import userInformation from "#tables/userInformation";
+import { INITIAL_POINTS } from "#config"
+import { Listener } from "#core/listener"
+import channelLimits from "#tables/channelLimits"
+import userInformation from "#tables/userInformation"
 
 /**
  * See the {@link https://ghom.gitbook.io/bot.ts/usage/create-a-listener listener guide} for more information.
  */
 export default new Listener({
-  event: "messageCreate",
-  description: "listens for users and sets mute status as needed",
-  async run(message) {
-    if (message.author.bot) return;
+	event: "messageCreate",
+	description: "listens for users and sets mute status as needed",
+	async run(message) {
+		if (message.author.bot) return
 
-    const channelInfo = await channelLimits.query
-      .where({
-        channel_id: message.channel.id,
-        guild_id: message.guild?.id!,
-      })
-      .first();
+		const channelInfo = await channelLimits.query
+			.where({
+				channel_id: message.channel.id,
+				guild_id: message.guild?.id!,
+			})
+			.first()
 
-    // No Channel Limits are Set.
-    if (!channelInfo) {
-      return;
-    }
-    // Sets the Cost for business.
-    const cost = message.content.length * channelInfo.cost;
+		// No Channel Limits are Set.
+		if (!channelInfo) {
+			return
+		}
+		// Sets the Cost for business.
+		const cost = message.content.length * channelInfo.cost
 
-    let isNewUser = false;
-    let userInfo = await userInformation.query
-      .where({
-        user_id: message.author.id,
-      })
-      .first();
-    if (!userInfo) {
-      isNewUser = true;
-      userInfo = {
-        user_id: message.author.id,
-        guild_id: message.guild?.id!,
-        allowance: INITIAL_POINTS,
-      };
-    }
-    // Makes adjustments to the user's allowance
-    userInfo.allowance -= cost;
+		let isNewUser = false
+		let userInfo = await userInformation.query
+			.where({
+				user_id: message.author.id,
+			})
+			.first()
+		if (!userInfo) {
+			isNewUser = true
+			userInfo = {
+				user_id: message.author.id,
+				guild_id: message.guild?.id!,
+				allowance: INITIAL_POINTS,
+			}
+		}
+		// Makes adjustments to the user's allowance
+		userInfo.allowance -= cost
 
-    console.log(
-      `${isNewUser} User ${message.author.id} has a cost of ${cost}, new allowance is ${userInfo.allowance}`
-    );
-    // If it dropped below user, apply the Muted role.
-    if (userInfo.allowance < 0) {
-      message.member?.roles.add(
-        message.guild?.roles.cache.find((r) => r.name === "Muted")!
-      );
-    }
+		console.log(
+			`${isNewUser} User ${message.author.id} has a cost of ${cost}, new allowance is ${userInfo.allowance}`,
+		)
+		// If it dropped below user, apply the Muted role.
+		if (userInfo.allowance < 0) {
+			message.member?.roles.add(
+				message.guild?.roles.cache.find((r) => r.name === "Muted")!,
+			)
+		}
 
-    // Update or Insert the user information.
-    if (isNewUser) {
-      await userInformation.query.insert(userInfo);
-    } else {
-      await userInformation.query.update(userInfo).where({
-        user_id: message.author.id,
-      });
-    }
-  },
-});
+		// Update or Insert the user information.
+		if (isNewUser) {
+			await userInformation.query.insert(userInfo)
+		} else {
+			await userInformation.query.update(userInfo).where({
+				user_id: message.author.id,
+			})
+		}
+	},
+})
--- src/slash/channelInit.ts
+++ src/slash/channelInit.ts
@@ -1,97 +1,97 @@
-import { ChannelType } from "discord.js";
-import { MAX_ALLOWANCE } from "#config";
-import { SlashCommand } from "#core/slash";
-import channelLimits from "#tables/channelLimits";
+import { ChannelType } from "discord.js"
+import { MAX_ALLOWANCE } from "#config"
+import { SlashCommand } from "#core/slash"
+import channelLimits from "#tables/channelLimits"
 
 const setupChannelCost = async (
-  guildId: string | null,
-  channelId: string,
-  cost: number | null
+	guildId: string | null,
+	channelId: string,
+	cost: number | null,
 ) => {
-  const limit = await channelLimits.query
-    .where({
-      guild_id: guildId!,
-      channel_id: channelId,
-    })
-    .first();
-  if (!limit) {
-    return await channelLimits.query.insert({
-      guild_id: guildId!,
-      channel_id: channelId,
-      cost: cost!,
-    });
-  }
-  return await channelLimits.query
-    .update({
-      cost: cost || 0,
-    })
-    .where({
-      guild_id: guildId!,
-      channel_id: channelId,
-    });
-};
+	const limit = await channelLimits.query
+		.where({
+			guild_id: guildId!,
+			channel_id: channelId,
+		})
+		.first()
+	if (!limit) {
+		return await channelLimits.query.insert({
+			guild_id: guildId!,
+			channel_id: channelId,
+			cost: cost!,
+		})
+	}
+	return await channelLimits.query
+		.update({
+			cost: cost || 0,
+		})
+		.where({
+			guild_id: guildId!,
+			channel_id: channelId,
+		})
+}
 
 /**
  * See the {@link https://ghom.gitbook.io/bot.ts/usage/create-a-command command guide} for more information.
  */
 export default new SlashCommand({
-  name: "channel-init",
-  description: "initializes a channel to marketing",
-  build: (builder) => {
-    builder.addNumberOption((option) => {
-      option
-        .setName("cost")
-        .setDescription("The cost per character in the channel")
-        .setRequired(true)
-        .setMinValue(0)
-        .setMaxValue(MAX_ALLOWANCE);
-      return option;
-    });
-  },
-  async run(interaction) {
-    if (interaction.user.id !== interaction.guild?.ownerId) {
-      return interaction.reply({
-        content: "Only the server owner can initialize channels.",
-        ephemeral: true,
-      });
-    }
+	name: "channel-init",
+	description: "initializes a channel to marketing",
+	build: (builder) => {
+		builder.addNumberOption((option) => {
+			option
+				.setName("cost")
+				.setDescription("The cost per character in the channel")
+				.setRequired(true)
+				.setMinValue(0)
+				.setMaxValue(MAX_ALLOWANCE)
+			return option
+		})
+	},
+	async run(interaction) {
+		if (interaction.user.id !== interaction.guild?.ownerId) {
+			return interaction.reply({
+				content: "Only the server owner can initialize channels.",
+				ephemeral: true,
+			})
+		}
 
-    console.log(
-      "Initializing channel:",
-      interaction.channelId,
-      "with cost:",
-      interaction.options.getNumber("cost")
-    );
-    // Conducts the Channel Setup
-    await setupChannelCost(
-      interaction.guildId,
-      interaction.channelId,
-      interaction.options.getNumber("cost")
-    );
+		console.log(
+			"Initializing channel:",
+			interaction.channelId,
+			"with cost:",
+			interaction.options.getNumber("cost"),
+		)
+		// Conducts the Channel Setup
+		await setupChannelCost(
+			interaction.guildId,
+			interaction.channelId,
+			interaction.options.getNumber("cost"),
+		)
 
-    // Creates Muted Role if it doesn't exist and assign it to the top of the Role Hierarchy.
-    let role = await interaction.guild?.roles.cache.find(
-      (r) => r.name === "Muted"
-    );
-    if (!role) {
-      role = await interaction.guild?.roles.create({
-        name: "Muted",
-        color: "Grey",
-      });
-      interaction.guild?.roles.setPosition(role, 0);
-    }
+		// Creates Muted Role if it doesn't exist and assign it to the top of the Role Hierarchy.
+		let role = await interaction.guild?.roles.cache.find(
+			(r) => r.name === "Muted",
+		)
+		if (!role) {
+			role = await interaction.guild?.roles.create({
+				name: "Muted",
+				color: "Grey",
+			})
+			interaction.guild?.roles.setPosition(role, 0)
+		}
 
-    // Sets Channel Specific Permissions for the `Muted` Role.
-    interaction.channel.type === ChannelType.GuildText &&
-      interaction.channel?.permissionOverwrites.create(role!, {
-        SendMessages: false, // Disallow sending messages for this role
-      });
+		// Sets Channel Specific Permissions for the `Muted` Role.
+		interaction.channel.type === ChannelType.GuildText &&
+			interaction.channel?.permissionOverwrites.create(role!, {
+				SendMessages: false, // Disallow sending messages for this role
+			})
 
-    return interaction.reply({
-      content: `Channel initialized successfully! Cost per Character set to: ${interaction.options.getNumber(
-        "cost"
-      )}`,
-      ephemeral: true,
-    });
-  },
-});
+		return interaction.reply({
+			content: `Channel initialized successfully! Cost per Character set to: ${interaction.options.getNumber(
+				"cost",
+			)}`,
+			ephemeral: true,
+		})
+	},
+})
